ab0ced37b4ea300d5d3fae7430f2425da5c4a609
Vagner Bom Jesus <vagneripg@gmail.com>
2022-05-12 18:36:39 +0100

    atualizar fich 4

--- ficha4/ex10.py
+++ ficha4/ex10.py
@@ -1,7 +1,17 @@
-jogadores = ["Ana", "Rui", "Telmo", "Beatriz", "Luisa", "Joana", "Manuel", "Pedro", "Paulo", "Paulino", "Maria", "Tereza", "Pedro", "Ricardo"]
-lista = []
-k = v = 0
 
-for i in range(0,len(jogadores),2):
+
+jogadoresP = ["Ana", "Rui", "Telmo", "Beatriz", "Luisa", "Joana", "Manuel"]
+jogadoresL = ["Pedro", "Paulo", "Paulino", "Maria", "Tereza", "Pedro", "Ricardo"]
+v = 0
+if(len(jogadoresL) != len(jogadoresP)):
+    if(len(jogadoresL) > len(jogadoresP)):
+        jogadoresP.append("Vagner")
+    else:
+        jogadoresL.append("Vagner")
+
+totalEquipa = jogadoresP.copy() + jogadoresL.copy()
+print(f'Jogadores Para o Torneio: {totalEquipa}')
+for i in range(0,len(jogadoresL),2):
     v+= 1
-    print(f"Equipa {v} = ['{jogadores[i]}', '{jogadores[i+1]}']")
+    print(f"Equipa {v} = ['{jogadoresL[i]}', '{jogadoresP[i]}']")
+
